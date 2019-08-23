49139b232a9e98fb1f15e64b661032143fef0497
RMOmd <rmo84791@gmail.com>
2019-08-23 23:11:56 +0300

    прописал координаты для APN

--- automate.py
+++ automate.py
@@ -6,18 +6,23 @@ time.sleep(3)
 device = os.popen("adb devices").read().split('\n', 1)[1].split("device")[0].strip()
 # connect to the selected device 127.0.0.1:62001
 print("Waiting for connection ...")
-connect = os.popen("adb connect " + device ).read()
-print(connect)
-#os.system("adb shell input tap 442 980")
-#time.sleep(3)
-#os.system("adb shell input tap 130 1232 130 190 100")
-#time.sleep(3)
-#os.system("adb shell input tap 143 1086")
-#time.sleep(3)
-#os.system("adb shell input tap 162 205")
-#time.sleep(3)
-#os.system("adb shell input tap 131 593")
-#time.sleep(3)
+os.system("adb shell input tap 261 1434") #mobile networks
+time.sleep(1)
+os.system("adb shell input tap 216 1030") #apn
+time.sleep(1)
+os.system("adb shell input tap 785 155") #add
+time.sleep(1)
+os.system("adb shell input tap 155 340") #name apn
+time.sleep(1)
+os.system('adb shell input text "idc"')
+os.system("adb shell input tap 895 732") #ok
+time.sleep(1)
+os.system("adb shell input tap 125 526") #apn
+time.sleep(1)
+os.system('adb shell input text "idc"')
+os.system("adb shell input tap 895 732") #ok
+time.sleep(3)
+#adb shell input text "Text_%s_tut"
 # swipe the page
 #time.sleep(5)
 #os.system("adb shell input tap 340 1030 340 650 100")
