4bf3d78eb57a5caa712200ca70a97464abce1a6f
RMOmd <rmo84791@gmail.com>
2019-08-23 21:27:38 +0300

    прописал координаты для включения аккаунта

--- automate.py
+++ automate.py
@@ -1,9 +1,20 @@
 import os
 import time
 
-os.system("adb shell input tap 340 1030")
+os.system("adb devices")
+time.sleep(3)
+os.system("adb shell input tap 442 980")
+time.sleep(3)
+os.system("adb shell input tap 130 1232 130 190 100")
+time.sleep(3)
+os.system("adb shell input tap 143 1086")
+time.sleep(3)
+os.system("adb shell input tap 162 205")
+time.sleep(3)
+os.system("adb shell input tap 131 593")
+time.sleep(3)
 # swipe the page
-time.sleep(5)
-os.system("adb shell input tap 340 1030 340 650 100")
+#time.sleep(5)
+#os.system("adb shell input tap 340 1030 340 650 100")
 # press back key
-os.system("adb shell input keyevent 4")
+#os.system("adb shell input keyevent 4")
