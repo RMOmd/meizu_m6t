b0512adc13005d510c745a9c4e1c2a2d5aba43a3
RMOmd <rmo84791@gmail.com>
2019-08-27 09:49:13 +0300

    объединил 2 скрипта, заливается build-prop, apn/spn, потом перезагруз, дожидается устройство, надо разблокировать экран, скрипт сам включит VoLTE, сбросит apn и зайдет в HiddenMenu

--- meizuM6t.py
+++ meizuM6t.py
@@ -1,5 +1,7 @@
 # Скрипт для настройки VoLTE на Meizu M6t под IDC
 import subprocess
+import os
+import time
 
 subprocess.call("adb push F:\\scriptM6t\\build.prop //sdcard/", shell=True)
 subprocess.call("adb push F:\\scriptM6t\\apns-conf.xml //sdcard/", shell=True)
@@ -14,3 +16,31 @@ procId.communicate('su\n'
                    'reboot\n'
                    'exit\n'.encode())
 print('Готово')
+subprocess.call("adb wait-for-device", shell=True)
+os.system("adb devices")
+time.sleep(60)
+os.popen("adb devices").read().split('\n', 1)[1].split("device")[0].strip()
+# connect to the selected device 127.0.0.1:62001
+print("Waiting for connection ...")
+os.system("adb shell input tap 435 990") #Заходим в настройки
+print("Заходим в настройки")
+time.sleep(1)
+os.system("adb shell input tap 186 262") #сим-карты и сети
+print("Заходим в сим-карты и сети.")
+time.sleep(1)
+os.system("adb shell input tap 631 949") #включаю вольте
+print("Включаю VoLTE")
+time.sleep(1)
+os.system("adb shell input tap 290 1228") #apn
+print("Заходим в точки доступа")
+os.system("adb shell input tap 615 110") #сброс апн
+print("Сбрасываем точки доступа")
+time.sleep(7)
+os.system("adb shell input tap 343 1390") #выход на рабочий стол
+print("Выходим на рабочий стол")
+time.sleep(1)
+os.system("adb shell input tap 99 1250") #звонилка
+print("Заходим в звонилку")
+time.sleep(1)
+os.system('adb shell input text "*#*#3646633#*#*"')
+print("Заходим в HiddenMenu")
