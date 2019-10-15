144b471f40441879cb8a03ea1b63f719aaa92829
Alex <rmo84791@gmail.com>
2019-10-15 22:32:30 +0300

    графическая оболочка
    доделал графическую оболочку, дописал лог операций, кнопка закрытия

--- meizuM6t.py
+++ meizuM6t.py
@@ -2,6 +2,40 @@
 import subprocess
 import os
 import time
+from tkinter import *
+
+try:
+    import Tkinter as tkinter
+except ImportError: # Python 3
+    import tkinter
+
+root = tkinter.Tk()
+text = tkinter.Text(root)
+text.pack()
+text.insert('end', u" копирую build.prop")
+root.after(2000, text.insert, 'end', u"\n копирую apns-conf.xml")
+root.after(3000, text.insert, 'end', u"\n копирую spn-conf.xml")
+root.after(5000, text.insert, 'end', u"\n монтирую system")
+root.after(7000, text.insert, 'end', u"\n заменяю build.prop, apns-conf.xml и spn-conf.xml")
+root.after(9000, text.insert, 'end', u"\n перегружаю устройство")
+root.after(11000, text.insert, 'end', u"\n ждем включения устройства")
+root.after(71000, text.insert, 'end', u"\n Заходим в настройки")
+root.after(72000, text.insert, 'end', u"\n Заходим в сим-карты и сети.")
+root.after(74000, text.insert, 'end', u"\n Включаю VoLTE")
+root.after(76000, text.insert, 'end', u"\n Заходим в точки доступа")
+root.after(78000, text.insert, 'end', u"\n Сбрасываем точки доступа.")
+root.after(85000, text.insert, 'end', u"\n Выходим на рабочий стол")
+root.after(87000, text.insert, 'end', u"\n Заходим в звонилку.")
+root.after(89000, text.insert, 'end', u"\n Заходим в HiddenMenu")
+
+
+root.geometry('400x450')
+root.config(bg='white')
+root.title('Meizu M6t IDC')
+
+quit_button = Button(root, text="Закрыть", bg="blue", fg="white", command=quit)
+quit_button.config(height = 2, width = 15)
+quit_button.pack(side=BOTTOM)
 
 subprocess.call("adb push F:\\scriptM6t\\build.prop //sdcard/", shell=True)
 subprocess.call("adb push F:\\scriptM6t\\apns-conf.xml //sdcard/", shell=True)
@@ -15,32 +49,23 @@ procId.communicate('su\n'
                    'cp -r /sdcard/spn-conf.xml /system/etc/\n'
                    'reboot\n'
                    'exit\n'.encode())
-print('Готово')
 subprocess.call("adb wait-for-device", shell=True)
 os.system("adb devices")
 time.sleep(60)
 os.popen("adb devices").read().split('\n', 1)[1].split("device")[0].strip()
 # connect to the selected device 127.0.0.1:62001
-print("Waiting for connection ...")
 os.system("adb shell input tap 435 990") #Заходим в настройки
-print("Заходим в настройки")
 time.sleep(1)
 os.system("adb shell input tap 186 262") #сим-карты и сети
-print("Заходим в сим-карты и сети.")
 time.sleep(1)
 os.system("adb shell input tap 631 949") #включаю вольте
-print("Включаю VoLTE")
 time.sleep(1)
 os.system("adb shell input tap 290 1228") #apn
-print("Заходим в точки доступа")
 os.system("adb shell input tap 615 110") #сброс апн
-print("Сбрасываем точки доступа")
 time.sleep(7)
 os.system("adb shell input tap 343 1390") #выход на рабочий стол
-print("Выходим на рабочий стол")
 time.sleep(1)
 os.system("adb shell input tap 99 1250") #звонилка
-print("Заходим в звонилку")
 time.sleep(1)
 os.system('adb shell input text "*#*#3646633#*#*"')
-print("Заходим в HiddenMenu")
+root.mainloop()
