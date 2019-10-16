f303886846bb7ebe91ed5522dbeabec39ddc7f66
mfkilla13 <41937042+mfkilla13@users.noreply.github.com>
2019-10-16 22:13:02 +0300

    VOLTE-2 Convert default design to Python, create main.py for start app

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 ### https://tproger.ru/translations/python-gui-pyqt/
 import sys  # sys нужен для передачи argv в QApplication
 import os  # Отсюда нам понадобятся методы для отображения содержимого директорий
-
 from PyQt5 import QtWidgets
-
 import design  # Это наш конвертированный файл дизайна
 
 class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
@@ -17,15 +15,15 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         self.setupUi(self)  # Это нужно для инициализации нашего дизайна
 
 
-    def browse_folder(self):
-        self.listWidget.clear()  # На случай, если в списке уже есть элементы
-        directory = QtWidgets.QFileDialog.getExistingDirectory(self, "Выберите папку")
-        # открыть диалог выбора директории и установить значение переменной
-        # равной пути к выбранной директории
-
-        if directory:  # не продолжать выполнение, если пользователь не выбрал директорию
-            for file_name in os.listdir(directory):  # для каждого файла в директории
-                self.listWidget.addItem(file_name)   # добавить файл в listWidget
+    # def browse_folder(self):
+    #     self.listWidget.clear()  # На случай, если в списке уже есть элементы
+    #     directory = QtWidgets.QFileDialog.getExistingDirectory(self, "Выберите папку")
+    #     # открыть диалог выбора директории и установить значение переменной
+    #     # равной пути к выбранной директории
+    #
+    #     if directory:  # не продолжать выполнение, если пользователь не выбрал директорию
+    #         for file_name in os.listdir(directory):  # для каждого файла в директории
+    #             self.listWidget.addItem(file_name)   # добавить файл в listWidget
 
 def main():
     app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
